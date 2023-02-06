ea36549d8b9d25a0e67a5fb352582a56ce325db8
andrea-buonocore <123959335+andrea-buonocore@users.noreply.github.com>
2023-02-06 14:51:18 +0100

    Giorno 1 - Week 2

---
+++ week2/day1/assets/js/index.js
@@ -0,0 +1,9 @@
+window.alert('Il mio primo popup!');
+window.alert('Sto studiando JS');
+
+
+
+
+document.write('<p>output scritto da javascript</p>');
+document.getElementById('p').innerHTML = '<h1>Questo lo ha scritto Javascript!</h1>';
+
---
+++ week2/day1/assets/js/naming.js
@@ -0,0 +1,10 @@
+var Pantera = 'pantera';
+var giraffaBassa = 'giraffa';
+var coniglio_grigio = 'coniglio';
+var CricetoRosso = 'criceto';
+
+console.log(giraffaBassa);
+console.log(coniglio_grigio);
+console.log(CricetoRosso);
+
+document.write(Pantera + ' scritto da javascript');
