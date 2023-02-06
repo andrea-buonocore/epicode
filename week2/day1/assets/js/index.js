af479f228ef12c3bbd84cbe3c57fc42c8c5355b3
andrea-buonocore <123959335+andrea-buonocore@users.noreply.github.com>
2023-02-06 15:37:24 +0100

    Giorno 1 - Week 2

--- week2/day1/assets/js/index.js
+++ week2/day1/assets/js/index.js
@@ -3,7 +3,7 @@ window.alert('Sto studiando JS');
 
 
 
+document.getElementById('body').innerHTML = '<h1>Questo lo ha scritto Javascript!</h1>';
 
 document.write('<p>output scritto da javascript</p>');
-document.getElementById('p').innerHTML = '<h1>Questo lo ha scritto Javascript!</h1>';
 
